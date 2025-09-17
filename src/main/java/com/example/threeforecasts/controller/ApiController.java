1ee3c20518dfca171d11a034c4bb79ae00e2f9be
James Hepburn <jameshepburn@Jamess-MacBook-Pro.local>
2025-09-17 14:49:05 -0600

    Player props

--- src/main/java/com/example/threeforecasts/controller/ApiController.java
+++ src/main/java/com/example/threeforecasts/controller/ApiController.java
@@ -2,7 +2,10 @@ package com.example.threeforecasts.controller;
 
 import com.example.threeforecasts.service.ApiService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class ApiController {
@@ -12,8 +15,8 @@ public class ApiController {
         this.apiService = apiService;
     }
 
-    @GetMapping("/receptions")
-    public void getPlayerReceptions () {
-        System.out.println (this.apiService.getPlayerReceptions ("aca5234c57e31b1931e51d2d0d6046f5"));
+    @GetMapping("/props/{market}")
+    public Map <String, Object> getPlayerProps (@PathVariable String market) {
+        return this.apiService.getPlayerProps ("aca5234c57e31b1931e51d2d0d6046f5", market);
     }
 }
--- src/main/java/com/example/threeforecasts/service/ApiService.java
+++ src/main/java/com/example/threeforecasts/service/ApiService.java
@@ -14,10 +14,10 @@ public class ApiService {
     private final RestTemplate restTemplate = new RestTemplate ();
     private final String baseUrl = "https://api.the-odds-api.com/v4/sports/americanfootball_nfl/";
 
-    public Map <String, Object> getPlayerReceptions (String eventId) {
+    public Map <String, Object> getPlayerProps (String eventId, String market) {
         String url = this.baseUrl +
                 "events/" + eventId + "/odds?apiKey=" + this.apiKey +
-                "&regions=us&markets=player_receptions&oddsFormat=american";
+                "&regions=us&markets=" + market + "&oddsFormat=american";
         return this.restTemplate.getForObject (url, Map.class);
     }
 }
